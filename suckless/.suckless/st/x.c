71c02e0e51bab769e31c9aedef1fa1c2cacba404
Daniel Guldberg Aaes <guldberg72@gmail.com>
2019-05-09 14:10:26 +0200

    shared clipboard

--- suckless/.suckless/st/x.c
+++ suckless/.suckless/st/x.c
@@ -631,6 +631,8 @@ setsel(char *str, Time t)
 	XSetSelectionOwner(xw.dpy, XA_PRIMARY, xw.win, t);
 	if (XGetSelectionOwner(xw.dpy, XA_PRIMARY) != xw.win)
 		selclear();
+
+	clipcopy(NULL);
 }
 
 void
@@ -648,7 +650,7 @@ brelease(XEvent *e)
 	}
 
 	if (e->xbutton.button == Button2)
-		selpaste(NULL);
+		clippaste(NULL);
 	else if (e->xbutton.button == Button1)
 		mousesel(e, 1);
 }
