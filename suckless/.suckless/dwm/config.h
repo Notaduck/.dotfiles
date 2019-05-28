bbf592698db8cdbd16b3b1ecadb7b09602e2cdc8
Daniel Guldbeg Aaes <guldberg72@gmail.com>
2019-05-28 16:43:47 +0200

    nord

--- suckless/.suckless/dwm/config.h
+++ suckless/.suckless/dwm/config.h
@@ -1,27 +1,16 @@
 /* See LICENSE file for copyright and license details. */
 #include "fibonacci.c"
+#include "./themes/nord.c"
 
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int borderpx  = 4;        /* border pixel of windows */
 static const unsigned int gappx     = 6;        /* gap pixel between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10" };
-static const char dmenufont[]       = "monospace:size=10";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
-static const char *colors[][3]      = {
-	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
-};
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+static const char *tags[] = { "WEB", "TERM", "DEV", "MEDIA", "RAND" };
 
 static const Rule rules[] = {
 	/* xprop(1):
---
+++ suckless/.suckless/dwm/themes/nord.c
@@ -0,0 +1,13 @@
+static const char *fonts[]          = { "Office Code Pro Medium:size=10" };
+static const char dmenufont[]       = "monospace:size=10";
+
+static const char col_gray1[]       = "#2e3440";
+static const char col_gray2[]       = "#3b4252";
+static const char col_gray3[]       = "#eceff4";
+static const char col_gray4[]       = "#bf616a";
+static const char col_cyan[]        = "#2e3440";
+static const char *colors[][3]      = {
+	/*               fg         bg         border   */
+	[SchemeNorm] = { col_gray3, col_gray2, col_gray2 },
+	[SchemeSel]  = { col_gray4, col_gray2, col_gray3  },
+};
