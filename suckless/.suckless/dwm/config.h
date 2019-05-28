749fbddb5ab89d2462b116655a27abaf2cc435dc
Daniel Guldbeg Aaes <guldberg72@gmail.com>
2019-05-28 13:01:19 +0200

    ..

--- suckless/.suckless/dwm/config.h
+++ suckless/.suckless/dwm/config.h
@@ -57,11 +57,13 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *networkmanager_dmenu[] = {"networkmanager_dmenu", NULL};
 static const char *termcmd[]  = { "st", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
+	{ MODKEY|ShiftMask,             XK_i,      spawn,          {.v = networkmanager_dmenu } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
