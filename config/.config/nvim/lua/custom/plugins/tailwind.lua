c877780d63363283dbc968bfb8ea16d8403a2688
Daniel Guldberg Aaes <daniel.aaes@indigounderwriters.com>
2024-09-04 16:01:22 +0200

    merge

---
+++ config/.config/nvim/lua/custom/plugins/tailwind.lua
@@ -0,0 +1,12 @@
+-- tailwind-tools.lua
+return {
+	"luckasRanarison/tailwind-tools.nvim",
+	name = "tailwind-tools",
+	build = ":UpdateRemotePlugins",
+	dependencies = {
+		"nvim-treesitter/nvim-treesitter",
+		"nvim-telescope/telescope.nvim", -- optional
+		"neovim/nvim-lspconfig", -- optional
+	},
+	opts = {}, -- your configuration
+}
