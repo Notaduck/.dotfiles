526a8201da993c71b861a4acbf5cc25a8ba17b59
Daniel Guldberg Aaes <daniel.aaes@indigounderwriters.com>
2024-12-05 10:03:16 +0100

    ...

--- nvim/.config/nvim/lua/custom/plugins/comments.lua
+++ nvim/.config/nvim/lua/custom/plugins/comments.lua
@@ -1,13 +1,5 @@
 -- File: ~/.config/nvim/lua/plugins/comment.lua
 
--- 1. Filetype Detection
-vim.api.nvim_create_autocmd("BufRead,BufNewFile", {
-	pattern = { ".env", ".env.*" },
-	callback = function()
-		vim.bo.filetype = "dotenv"
-	end,
-})
-
 -- 2. Plugin Configuration
 return {
 	"numToStr/Comment.nvim",
