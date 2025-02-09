48cee202603cee9671a1b5930b0ddef1c03b9666
Daniel Guldberg Aaes <daniel.aaes@indigounderwriters.com>
2025-02-09 20:47:13 +0100

    Remove existing nvim/.config/nvim folder from index to add submodule

--- nvim/.config/nvim/lua/custom/plugins/zen.lua
+++
@@ -1,9 +0,0 @@
--- Lua
-return {
-	"folke/zen-mode.nvim",
-	opts = {
-		-- your configuration comes here
-		-- or leave it empty to use the default settings
-		-- refer to the configuration section below
-	},
-}
